1a01de52ff15ce5997b2a287b3f9c307f345ce08
FomGr <127923465+FomGr@users.noreply.github.com>
2023-03-21 10:49:03 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.slaider-main-block', {
+	// Optional parameters
+	loop: true,
+	  // Navigation arrows
+	  navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	 },
+ });
